824995408e99556b520062ed8e35850c3a9aaff9
stranded-fish <zhouyulancq@foxmail.com>
2021-02-27 00:08:33 +0800

    init project

---
+++ src/main/java/com/yulan/distributedimageserver/DistributedImageServerApplication.java
@@ -0,0 +1,13 @@
+package com.yulan.distributedimageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedImageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedImageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yulan/distributedimageserver/DistributedImageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulan.distributedimageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedImageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
