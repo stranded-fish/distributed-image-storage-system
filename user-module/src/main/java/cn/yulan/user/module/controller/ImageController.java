28892d6ca03cce6c91e4da2549b85d242808a9de
stranded-fish <zhouyulancq@foxmail.com>
2021-03-09 16:21:21 +0800

    completed the demo

--- user-module/src/main/java/cn/yulan/user/module/controller/ImageController.java
+++ user-module/src/main/java/cn/yulan/user/module/controller/ImageController.java
@@ -1,9 +1,14 @@
 package cn.yulan.user.module.controller;
 
 import cn.yulan.user.module.result.BaseResult;
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcClient;
+import com.googlecode.protobuf.format.JsonFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import cn.yulan.storage.module.server.service.ExampleProto;
+import cn.yulan.storage.module.server.service.ExampleService;
 
 import java.io.File;
 
@@ -32,10 +37,9 @@ public class ImageController {
     public BaseResult<Boolean> uploadImage(@RequestParam("uploadImg") MultipartFile uploadImg) {
         BaseResult<Boolean> result = new BaseResult<>();
 
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("start save image in local file system");
+        System.out.println("...");
+        System.out.println("...");
 
         String path = "/root/pic";
         String fileName = uploadImg.getOriginalFilename();
@@ -49,11 +53,46 @@ public class ImageController {
             e.printStackTrace();
         }
 
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("end save image");
+        System.out.println("...");
+        System.out.println("...");
+
+        System.out.println();
+
+        System.out.println("start save path in raft clusters");
+        System.out.println("...");
+        System.out.println("...");
+
+        String ipPorts = "list://127.0.0.1:8051,127.0.0.1:8052,127.0.0.1:8053";
+        String key = fileName;
+        String value = path + "/" + fileName;
+
+        // init rpc client
+        RpcClient rpcClient = new RpcClient(ipPorts);
+        ExampleService exampleService = BrpcProxy.getProxy(rpcClient, ExampleService.class);
+        final JsonFormat jsonFormat = new JsonFormat();
+
+        // set
+        if (value != null) {
+            ExampleProto.SetRequest setRequest = ExampleProto.SetRequest.newBuilder()
+                    .setKey(key).setValue(value).build();
+            ExampleProto.SetResponse setResponse = exampleService.set(setRequest);
+            System.out.printf("set request, key=%s value=%s response=%s\n",
+                    key, value, jsonFormat.printToString(setResponse));
+        } else {
+            // get
+            ExampleProto.GetRequest getRequest = ExampleProto.GetRequest.newBuilder()
+                    .setKey(key).build();
+            ExampleProto.GetResponse getResponse = exampleService.get(getRequest);
+            System.out.printf("get request, key=%s, response=%s\n",
+                    key, jsonFormat.printToString(getResponse));
+        }
+
+
+        System.out.println("end save path");
+        System.out.println("...");
+        System.out.println("...");
+
 
 //        editHomePageService.saveTotalLayoutInfo(totalLayoutInfoBean, result);
         return result;
