391911f70a06ac1fc4e62c676ef99ca3065cc79b
stranded-fish <zhouyulancq@foxmail.com>
2021-03-09 09:52:02 +0800

    init project

---
+++ user-module/src/test/java/cn/yulan/user/module/UserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yulan.user.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
